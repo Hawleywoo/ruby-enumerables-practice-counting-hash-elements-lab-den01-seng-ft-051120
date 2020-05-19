5d58e2c5f8424b58c57670b1ddfb186c353863f8
hawleywoo <alhawley21@gmail.com>
2020-05-19 17:39:52 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,27 @@
+require "pry"
+
 def count_elements(array)
   # code goes here
+  #
+  new_hash = array.reduce({}) do |new_hash, element|
+    if new_hash[element]
+      new_hash[element] = new_hash[element] + 1
+      #binding.pry
+      new_hash
+    else
+      new_hash[element] = 1
+      new_hash
+    end
+  end
+ # binding.pry
+ new_hash
 end
+#count_elements(['stuff','other stuff','some more stuff','stuff' ])
  
+
+
+cats = ['stuff','stuff','stuff' ]
+
+
+prac = cats.map(&:length)
+#binding.pry
